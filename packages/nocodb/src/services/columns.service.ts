22a7d459afc801719c8b73c8053193f93a4fc84b
DarkPhoenix2704 <anbarasun123@gmail.com>
2026-04-23 07:52:51 +0000

    fix: more corrections

--- packages/nocodb/src/services/columns.service.ts
+++ packages/nocodb/src/services/columns.service.ts
@@ -3723,6 +3723,7 @@ export class ColumnsService implements IColumnsService {
       user: UserType;
       forceDeleteSystem?: boolean;
       skipLinkPlaceholder?: boolean;
+      skipTrash?: boolean;
       reuse?: ReusableParams;
       columnWebhookManager?: ColumnWebhookManager;
     },
